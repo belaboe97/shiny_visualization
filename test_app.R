3a3b5c2d07847f778caa1f5ec369095830275658
Bela <66631119+belaboe97@users.noreply.github.com>
2022-01-25 18:38:32 +0100

    reactive button and error handling

--- test_app.R
+++ test_app.R
@@ -10,7 +10,7 @@ library(DT)
 library(purrrlyr)
 library(shinyBS)
 library("reshape2")
-
+library(htmltools)
 #install.packages("janitor")
 library(janitor)
 library(dplyr)
@@ -45,11 +45,12 @@ filterList = list(
 
 get_properties = function (flsit){return(names(filterList[filterList==T]))}
 
-get_data_mean = function(data){
+get_data_mean = function(data,years){
   clean %>% 
-    select(get_properties(mean_val_song_data), year) %>% 
+    select(get_properties(data), year) %>% 
     slice_rows("year") %>% 
-    dmap(mean) -> 
+    dmap(mean) %>% 
+    filter(year > years[1] & year < years[2])-> 
     df_song_data
   df_song_data =  melt(df_song_data, id="year")
   return(df_song_data)
@@ -80,7 +81,7 @@ clean %>%
   select(c("name", "artists", "popularity", "release_date", "year")) ->
   df_song_metadata
 
-df_song_data_mean = get_data_mean(clean)
+df_song_data_mean = get_data_mean(clean,c("1921","2020"))
 df_song_data_median = get_data_median(clean)
 
 
@@ -126,13 +127,14 @@ summary(df_song_metadata)
 
 # User interface ----
 ui <- fluidPage(
-  theme = shinytheme("united"),
+  #theme = shinytheme("united"),
   
   tags$head(
     tags$style(type="text/css", ".irs { width: 100% }"),
     tags$style(HTML('#btn_mean{margin:1rem; width:100%}')),
     tags$style(HTML('#btn_median{margin:1rem; width:100%}')),
     tags$style(HTML('#btn_boxplot{margin:1rem; width:100%}')),
+    tags$style(HTML('.btn-buttom {width: 15%}')),
   ),
 
    fluidRow(
@@ -149,7 +151,7 @@ ui <- fluidPage(
               column(12,style="",
                      bsButton("btn_mean", "average", 
                                   icon("align-center"), 
-                                  class="sideBtns btn btn-primary",
+                                  class="btn btn-primary",
                                   style="margin:1rem; width:100%", 
                                   ),
 
@@ -161,7 +163,7 @@ ui <- fluidPage(
                     
                     bsButton("btn_boxplot", "boxplot", 
                                  icon("box-open"), 
-                                 class="btn",
+                                 class="btn-buttom",
                                  style="margin:1rem; width:100%", 
                     ),
             ))),
@@ -170,23 +172,39 @@ ui <- fluidPage(
    
     column(2),
     column(10,style="margin-top:0.5rem,left:3rem",
-             actionButton("acoust", "acousticness", 
-                          icon("guitar"), 
-                          class="btn btn-success"),
+             actionButton("acoust", "acoustics", 
+                          icon("guitar"),
+                          style="background-color:#F2755B",
+                          class="btn-buttom"
+                          ),
            
-             actionButton("danceab", "danceability", 
-                          icon("volume-up"), 
-                          class="btn btn-danger"
+             actionButton("danceab", "dancy", 
+                          icon("drum-steelpan"), 
+                          style="background-color:#F2A65B",
+                          class="btn-buttom"
                           ),
            
              actionButton("dura", "duration", 
                           icon("hourglass-half"), 
-                          class="btn btn-warning"),
+                          style="background-color:#F2DD5B",
+                          class="btn-buttom"
+                          ),
            
              actionButton("engy", "energy", 
                           icon("bolt"), 
-                          class="btn btn-info"),
-           
+                          style="background-color:#BEF25B",
+                          class="btn-buttom"
+                          ),
+             actionButton("loud", "loudness", 
+                        icon("volume-up"), 
+                        style="background-color:#60F25B",
+                        class="btn-buttom"
+                        ),
+             actionButton("tempo", "tempo", 
+                        icon("tachometer-alt"), 
+                        style="background-color:#5BF2DA",
+                        class="btn-buttom"
+                        ),
     )
 
   )
@@ -194,18 +212,33 @@ ui <- fluidPage(
 )
 
 
-d2 = get_data_mean(clean)
+d1 = get_data_median(clean)
 
-print(d2)
+d2 = get_data_mean(clean,c("1921","2020"))
+
+length(get_properties(clean))
+
+switch_btn = function (value_string){
+
+  if(length(get_properties(clean)) > 1 || filterList[[value_string]] == F ){
+    if(filterList[[value_string]]) filterList[value_string]<<- F else filterList[value_string] <<- T 
+    print(filterList[[value_string]])
+    }
+  else{
+    showNotification("Atleast one graph must be displayed", type="error")
+  }
+}
+#print(d1)
 # Server logic ----
 server <- function(input, output,session) {
   
-
-  reactv = reactiveValues(plot_data = d2 , color="red")
-  
   year_avg_year = reactive({ input$year_avg_year })
-
+  reactv = reactiveValues(plot_data = get_data_mean(clean,c("1921","2020")) , color="red")
   
+  observeEvent(input$year_avg_year,{
+    reactv$plot_data <- get_data_mean(clean,input$year_avg_year)
+  })
+
   observeEvent(input$btn_mean, {
     reactv$plot_data = mean_val_song_data
     reactv$color="red"
@@ -215,7 +248,7 @@ server <- function(input, output,session) {
   })
   
   observeEvent(input$btn_median, {
-    data$plot_data = median_val_song_data
+    data$plot_data = d1
     reactv$color="blue"
     updateButton(session,'btn_median',style = "primary")
     updateButton(session,'btn_boxplot',style = "secondary")
@@ -232,29 +265,46 @@ server <- function(input, output,session) {
   
   
   observeEvent(input$acoust, {
-               if(filterList$acousticness) filterList$acousticness <<- F else filterList$acousticness <<- T
-               reactv$plot_data <- get_data_mean(clean)})
+    switch_btn("acousticness")          
+    reactv$plot_data <- get_data_mean(clean,year_avg_year())})
   
   observeEvent(input$danceab, {
-               if(filterList$danceability) filterList$danceability <<- F else filterList$danceability <<- T
-               reactv$plot_data <- get_data_mean(clean)})
+    switch_btn("danceability")   
+    reactv$plot_data <- get_data_mean(clean,year_avg_year())})
   
   observeEvent(input$dura, {
-               if(filterList$duration_ms) filterList$duration_ms <<- F else filterList$duration_ms <<- T
-               reactv$plot_data <- get_data_mean(clean)})
+    switch_btn("duration_ms")   
+    reactv$plot_data <- get_data_mean(clean,year_avg_year())})
   
   observeEvent(input$engy, {
-               if(filterList$energy) filterList$energy <<- F else filterList$energy <<- T
-               reactv$plot_data <- get_data_mean(clean)})
+   switch_btn("energy")   
+   reactv$plot_data <- get_data_mean(clean,year_avg_year())})
+  
+  observeEvent(input$loud, {
+   switch_btn("loudness") 
+   reactv$plot_data <- get_data_mean(clean,year_avg_year())})
+  
+  observeEvent(input$tempo, {
+   switch_btn("tempo") 
+   reactv$plot_data <- get_data_mean(clean,year_avg_year())})
   
    output$avg_per_year_plot = renderPlot({ 
 
    ggplot(
       reactv$plot_data,aes(x=year,y=value, colour=variable))+
-      geom_point(shape=18, color=reactv$color)+
+      geom_point(shape=18)+
+       scale_color_manual(name = "Properties", 
+                          values = c("acousticness" = "#F2755B", 
+                                     "danceability" = "#F2A65B", 
+                                     "duration_ms" = "#F2DD5B",
+                                     "energy" = "#BEF25B", 
+                                     "loudness" = "#60F25B", 
+                                     "tempo" = "#5BF2DA"))+
       geom_smooth()
   })
 }
+
+
 
   
   
